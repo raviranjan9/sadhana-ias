3e346f7a151412e958f6eaff5e1eb441e770d377
raviranjan <raviranjancp@gmail.com>
2022-08-27 19:39:55 +0530

    final commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-const PORT = process.config.env || 3000;
-const staticPath = path.join(__dirname, "/public");
+const PORT = process.env.PORT || 3000;
+const staticPath = path.join(__dirname, "./public");
 
 app.use(express.static(staticPath));
 app.get("/", (req, res) => {
